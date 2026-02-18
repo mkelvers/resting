a434834ef1fc9b9015e422e5d3241d5cad3302b3
mkelvers <mikkelelvers@outlook.com>
2026-02-18 18:24:24 +0100

    safe load envs

--- index.php
+++ index.php
@@ -4,12 +4,15 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Dotenv\Dotenv;
 use App\Routes;
 
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+// Only load .env file if environment variables are not already set (e.g., in production)
+if (!isset($_ENV['DB_HOST'])) {
+  $dotenv = Dotenv::createImmutable(__DIR__);
+  $dotenv->safeLoad();
+}
 
 $app = new Comet\Comet([
-    'host' => '127.0.0.1',
-    'port' => $_ENV['PORT'],
+    'host' => '0.0.0.0',
+    'port' => $_ENV['PORT'] ?? 8080,
 ]);
 
 Routes::register($app);
