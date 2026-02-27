edd74bad114c678ea2194c4be9b1bc55ec31ce92
mkelvers <mikkelelvers@outlook.com>
2026-02-27 11:07:06 +0100

    bit more

--- routes/Home.php
+++ routes/Home.php
@@ -6,7 +6,11 @@ use Comet\Request;
 use Comet\Response;
 
 $app->get('/', function (Request $request, Response $response) {
-  return $response->with(['message' => 'hello world'], 200);
+  return $response->with([
+    'message' => 'hello world',
+    'version' => '1.0.0',
+    'doc' => $this->baseUrl . '/docs'
+  ], 200);
 });
 
 $app->get('/favicon.ico', function ($request, $response) {
